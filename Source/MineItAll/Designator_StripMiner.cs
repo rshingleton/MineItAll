23afc3ad43ea35f70c4922143a349a32e8e9e9a4
Russell Shingleton <russellshomes@aol.com>
2016-12-20 21:32:27 -0500

    Initial Commit
    Rimworld Mining Mod

---
+++ Source/MineItAll/Designator_StripMiner.cs
@@ -0,0 +1,86 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MineItAll
+{
+    public class Designator_StripMiner : Designator_Mine
+    {
+        private IntVec3 startPoint;
+
+        private int spacing = 4;
+
+        private int spacingY = 4;
+
+        public override int DraggableDimensions
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public StripMiner()
+        {
+            this.defaultLabel = "Strip Miner";
+            this.icon = ContentFinder<Texture2D>.Get("StripMine", true);
+            this.defaultDesc = "Drag an area to strip mine. Use 8,4,5,6 on your !NUMBLOCK! to change spacing between strips.";
+            this.useMouseIcon = true;
+            this.soundDragSustain = SoundDefOf.DesignateDragStandard;
+            this.soundDragChanged = SoundDefOf.DesignateDragStandardChanged;
+            this.soundSucceeded = SoundDefOf.DesignateMine;
+            this.tutorTag = "DesignatorMine";
+        }
+
+        public override void SelectedUpdate()
+        {
+            base.SelectedUpdate();
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.startPoint = UI.MouseCell();
+            }
+        }
+
+        public override AcceptanceReport CanDesignateCell(IntVec3 c)
+        {
+            IntVec3 intVec = c - this.startPoint;
+            if ((intVec.x % this.spacing == 0) || (intVec.z % this.spacingY == 0))
+            {
+                return base.CanDesignateCell(c);
+            }
+            else
+            {
+                return AcceptanceReport.WasRejected;
+            }
+        }
+
+        public override void DrawMouseAttachments()
+        {
+            base.DrawMouseAttachments();
+            if (Input.GetKeyUp((KeyCode)264))
+            {
+                this.spacingY++;
+            }
+            if (Input.GetKeyUp((KeyCode)262))
+            {
+                this.spacing++;
+            }
+            if (Input.GetKeyUp((KeyCode)260) && this.spacing > 4)
+            {
+                this.spacing--;
+            }
+            if (Input.GetKeyUp((KeyCode)261) && this.spacingY > 4)
+            {
+                this.spacingY--;
+            }
+            //Log.Message(this.spacing + ", " + this.spacingY);
+        }
+
+        public override void ProcessInput(Event ev)
+        {
+            base.ProcessInput(ev);
+            this.spacing = 4;
+            this.spacingY = 4;
+        }
+    }
+}
---
+++ Source/MineItAll/MineItAll/Designator_MineBrush.cs
@@ -0,0 +1,95 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace MineItAll
+{
+    public class Designator_MineBrush : Designator_Mine
+    {
+        private float radius = 1f;
+        private Material circleMat = MaterialPool.MatFrom("CircleTex", true);
+
+        public override int DraggableDimensions
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public Designator_MineBrush()
+        {
+            this.defaultLabel = "Mine Brush";
+            this.icon = ContentFinder<Texture2D>.Get("MineBrush", true);
+            this.defaultDesc = "Finally a brush for mining. Use q and e to change size.";
+            this.useMouseIcon = true;
+            this.soundDragSustain = SoundDefOf.DesignateDragStandard;
+            this.soundDragChanged = SoundDefOf.DesignateDragStandardChanged;
+            this.soundSucceeded = SoundDefOf.DesignateMine;
+            this.tutorTag = "DesignatorMine";
+        }
+
+        public override void DesignateSingleCell(IntVec3 loc)
+        {
+            Designator_Mine designator_Mine = new Designator_Mine();
+            designator_Mine.DesignateMultiCell(this.designateAt(loc));
+        }
+
+        private List<IntVec3> designateAt(IntVec3 here)
+        {
+            return GenRadial.RadialCellsAround(here, this.radius, true).ToList<IntVec3>();
+        }
+
+        public override AcceptanceReport CanDesignateCell(IntVec3 c)
+        {
+            return AcceptanceReport.WasAccepted;
+        }
+
+        public override void DrawMouseAttachments()
+        {
+			base.DrawMouseAttachments();
+			bool flag = Input.GetKeyUp((KeyCode)113) && this.radius > 0f;
+			if (flag)
+			{
+				this.radius -= 0.1f;
+			}
+			bool flag2 = Input.GetKeyUp((KeyCode)101) && this.radius < 30f;
+			if (flag2)
+			{
+				this.radius += 0.1f;
+			}
+        }
+
+        public override void SelectedUpdate()
+        {
+            base.SelectedUpdate();
+            IntVec3 pos = UI.MouseCell();
+            this.drawCircle(pos);
+        }
+
+        private void drawCircle(IntVec3 pos)
+        {
+            //Log.Message("Draw Circle");
+            foreach (IntVec3 current in GenRadial.RadialCellsAround(pos, this.radius, true))
+            {
+                Graphics.DrawMesh(MeshPool.plane10, current.ToVector3ShiftedWithAltitude(28f), Quaternion.identity, this.circleMat, 0);
+            }
+        }
+
+        public override void ProcessInput(Event ev)
+        {
+            bool flag = Find.DesignatorManager.SelectedDesignator != null && this.radius == 7.9f;
+            if (flag)
+            {
+                this.radius = DefDatabase<ThingDef>.GetNamed("SunLamp", true).specialDisplayRadius;
+            }
+            else
+            {
+                this.radius = 7.9f;
+            }
+            base.ProcessInput(ev);
+        }
+    }
+}
